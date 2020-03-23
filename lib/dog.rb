19645ee15e5acf622f5ef26c2b6a0bec08cf07b3
Roberto Livi <robertomlivi@outlook.com>
2020-03-23 00:30:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,8 +46,8 @@ class Dog
       :name => values[1],
       :breed => values[2]
     }
-    
-    DB[:conn].execute(attributes)
+    result = (DB[:conn].execute(attributes)
+    Dog.new(result)
   end
     
   
